d3eb1c9c8a862e5d08bb7efdd553bae0819fee57
Kelvin Yip <autilite@gmail.com>
2015-12-14 03:43:20 -0800

    Fix volume.sh comparison

--- i3/.i3/scripts/volume.sh
+++ i3/.i3/scripts/volume.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 MUTE=$(amixer get Master | grep Left: | cut -d " " -f8 )
 VOLUME=$(amixer get Master | grep Left: | cut -d " " -f7 | tr -d '[%]')
-if [ $MUTE == "[off]" ]
+if [ $MUTE == "[off]" ] || [ $VOLUME == 0 ]
 then
     echo ""
 else
-    if [ $VOLUME > 50 ]
+    if [ $VOLUME -ge 50 ]
     then
         echo $VOLUME"% "
     else

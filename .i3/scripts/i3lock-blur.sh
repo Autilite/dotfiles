504f755121130195caf87f68266d49d1d77e10e7
autilite <autilite@gmail.com>
2017-12-25 19:47:24 -0800

    Move i3lock-blur to i3 dir
    - Add lock.png
    - Change image location to one generated with mktemp

--- .i3/scripts/i3lock-blur.sh
+++ .i3/scripts/i3lock-blur.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-TEMP=/tmp/screen.png
-LOCK=$HOME/Pictures/lock.png
+TEMP=$(mktemp --suffix=.png)
+LOCK=$HOME/.i3/lock.png
 RES=$(xrandr | grep '*' | awk '{print $1}')
  
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK \
   -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
   -vframes 1 $TEMP \
   -loglevel quiet
+
 i3lock -e -i $TEMP
+
 rm $TEMP
